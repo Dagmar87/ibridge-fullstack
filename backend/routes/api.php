5e04e8d1bd4817f8bbedeb48e61e7bf5ec79d3d7
Dagmar87 <jdfssobrinho@hotmail.com>
2026-02-27 16:22:53 -0300

    Criação dos Endpoints da API

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Chamada;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,46 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/resumo', function () {
+    return Chamada::selectRaw('lista_id,
+        COUNT(*) as total,
+        SUM(CASE WHEN situacao_id = 4 THEN 1 ELSE 0 END) as fechamentos')
+        ->groupBy('lista_id')
+        ->with('lista')
+        ->get();
+});
+
+Route::get('/top-operadores', function () {
+    return Chamada::selectRaw('operador_id,
+        COUNT(*) as total_fechamentos')
+        ->where('situacao_id', 4)
+        ->groupBy('operador_id')
+        ->orderByDesc('total_fechamentos')
+        ->with('operador')
+        ->limit(10)
+        ->get();
+});
+
+Route::get('/top-listas', function () {
+    return Chamada::selectRaw('lista_id,
+        COUNT(*) as total_fechamentos')
+        ->where('situacao_id', 4)
+        ->groupBy('lista_id')
+        ->orderByDesc('total_fechamentos')
+        ->with('lista')
+        ->limit(10)
+        ->get();
+});
+
+Route::get('/top-campanhas', function () {
+    return Chamada::selectRaw('campanha_id,
+        COUNT(*) as total_fechamentos')
+        ->where('situacao_id', 4)
+        ->groupBy('campanha_id')
+        ->orderByDesc('total_fechamentos')
+        ->with('campanha')
+        ->limit(10)
+        ->get();
 });
