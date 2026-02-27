a2f6a5332477ade716f9b718990ec70c01328c6a
Dagmar87 <jdfssobrinho@hotmail.com>
2026-02-27 15:58:59 -0300

    Configuração dos Models (Relacionamentos) do arquivo Chamada.php

--- backend/app/Chamada.php
+++ backend/app/Chamada.php
@@ -6,5 +6,35 @@ use Illuminate\Database\Eloquent\Model;
 
 class Chamada extends Model
 {
-    //
+	public $timestamps = false;
+	protected $fillable = [
+		'id',
+		'datahora',
+		'contato_id',
+		'lista_id',
+		'campanha_id',
+		'operador_id',
+		'situacao_id',
+		'categoria_id'
+	];
+
+	public function operador()
+	{
+		return $this->belongsTo(Operador::class);
+	}
+
+	public function lista()
+	{
+		return $this->belongsTo(Lista::class);
+	}
+
+	public function campanha()
+	{
+		return $this->belongsTo(Campanha::class);
+	}
+
+	public function situacao()
+	{
+		return $this->belongsTo(Situacao::class);
+	}
 }
