f6a7d7a0d389dba6948bec66d27643204ddf3628
Dagmar87 <jdfssobrinho@hotmail.com>
2026-03-02 10:06:57 -0300

    Configuração Global do Chart.js

--- dashboard-frontend/src/main.jsx
+++ dashboard-frontend/src/main.jsx
@@ -3,6 +3,26 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Tooltip,
+  Legend
+} from "chart.js";
+
+import ChartDataLabels from "chartjs-plugin-datalabels";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Tooltip,
+  Legend,
+  ChartDataLabels
+);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
